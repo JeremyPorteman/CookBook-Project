fd3969c34f120c3e0d8e4c2aa36d25a7b8d5d4f6
JeremyPorteman <148571848+JeremyPorteman@users.noreply.github.com>
2024-11-29 15:30:34 +0100

    initial commit

---
+++ CookBook/CookBook/CookBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookBookApp.swift
+//  CookBook
+//
+//  Created by Jérémy PORTEMAN on 29/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct CookBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CookBook/CookBookTests/CookBookTests.swift
@@ -0,0 +1,17 @@
+//
+//  CookBookTests.swift
+//  CookBookTests
+//
+//  Created by Jérémy PORTEMAN on 29/11/2024.
+//
+
+import Testing
+@testable import CookBook
+
+struct CookBookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
